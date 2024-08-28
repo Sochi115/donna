a203febd641d8ff42516c343fb6ea7b8e748a726
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2024-08-28 09:36:11 +0700

    All + complete flag for delete
    Added all and complete flags for the delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -14,12 +15,40 @@ import (
 var deleteCmd = &cobra.Command{
 	Use:   "delete [ID]",
 	Short: "Deletes task",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	Long: `Deletes task with the given [ID].
   Deleted tasks can NOT be recovered`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			cmd.Help()
+			if cmd.Flags().Lookup("all").Changed {
+				fmt.Print("Do you want to delete ALL tasks? [y/n]: ")
+				var confirmation string
+				fmt.Scanln(&confirmation)
+
+				if strings.EqualFold(confirmation, "y") {
+					writeTasksToCsv(make([]Task, 0))
+					fmt.Println("Successfully deleted all tasks")
+					return
+				}
+				fmt.Println("Cancelled task")
+				return
+			}
+
+			if cmd.Flags().Lookup("completed").Changed {
+				fmt.Print("Do you want to delete all completed tasks? [y/n]: ")
+				var confirmation string
+				fmt.Scanln(&confirmation)
+
+				if strings.EqualFold(confirmation, "y") {
+					processTasks(fetchTasksAsList())
+					writeTasksToCsv(incompletedTasks)
+					fmt.Println("Successfully deleted all completed tasks")
+					return
+				}
+				fmt.Println("Cancelled task")
+				return
+
+			}
 		} else {
 			id, err := strconv.Atoi(args[0])
 			if err != nil {
@@ -34,6 +63,11 @@ var deleteCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
+	var all bool
+	var completed bool
+
+	deleteCmd.Flags().BoolVarP(&all, "all", "a", false, "delete all tasks")
+	deleteCmd.Flags().BoolVarP(&completed, "completed", "c", false, "delete all completed tasks")
 }
 
 func deleteTask(id int) {
